5f1cec6ab4f2ce1fab8b5fd03dd9dee64c19a6d5
Jonito <joniyogakusuma2005@gmail.com>
2026-03-19 12:06:12 +0700

    Praktikum-2.2

---
+++ database/migrations/2026_03_19_050228_create_m_barang_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('m_barang', function (Blueprint $table) {
+            $table->id('barang_id');
+            $table->unsignedBigInteger('kategori_id')->index();
+            $table->string('barang_kode', 15)->unique();
+            $table->string('barang_nama', 100);
+            $table->integer('harga_beli');
+            $table->integer('harga_jual');
+            $table->timestamps();
+
+            $table->foreign('kategori_id')
+                ->references('id')
+                ->on('m_kategori')
+                ->cascadeOnUpdate()
+                ->restrictOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_barang');
+    }
+};
