46ed17bffd9782a121e64b67a12733859bb201e6
Jonito <joniyogakusuma2005@gmail.com>
2026-04-14 22:24:51 +0700

    j4p2.6

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,11 @@ Route::get('/about', function () {
 // =============================================
 
 Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 
 Route::get('/user/{name?}', function ($name='John') {
     return 'Nama saya '.$name;
